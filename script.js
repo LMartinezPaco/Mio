7323328482fdd394917de216e1932121982f0e44
Laura Michelle <143129865+LMartinezPaco@users.noreply.github.com>
2026-04-07 00:55:31 -0300

    Crear script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+    function partidanueva(){
+    document.getElementById("menu").style.display="none";
+    document.getElementById("juego").style.display="block";
+    document.getElementById("texto").innerText=" ";
+    }

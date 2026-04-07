fb287fb3ecc6c5c23acb06044264474ce264e298
Laura Michelle <143129865+LMartinezPaco@users.noreply.github.com>
2026-04-07 17:53:33 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const escenario = {};
 
 const capitulo = {};
 
+let capprimero = "Capitulo 1";
+
 
 function partidanueva(){
     document.getElementById("menu").style.display="none";
     document.getElementById("juego").style.display="block";
-    document.getElementById("texto").innerText=" ";
-    }
+    document.getElementById("texto").innerText=
+}
